28ccf0a4dfef207f77c0a37d79bfab774a903c21
DH Services <dhservices1997@gmail.com>
2026-02-26 22:07:19 +0100

    Update vite config and build assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,7 @@ import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
-
-
-
-
-export default defineConfig(({ command }) => ({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        // N'exécute wayfinder qu'en mode 'serve' (dev), pas en 'build' (prod)
-        command === 'serve' ? wayfinder({ formVariants: true }) : null,
-    ].filter(Boolean), // Filtre les valeurs nulles
-}));
-/* export default defineConfig({
+export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
@@ -41,4 +27,4 @@ export default defineConfig(({ command }) => ({
     server: {
         host: '127.0.0.1',
     },
-});*/
+});
